b83d86da6f610dffe3cf7094a6a02d9bfbc5f072
unknown <flowershirtsman@gmail.com>
2019-08-25 16:07:48 -0400

    work

--- public/assets/js/household.js
+++ public/assets/js/household.js
@@ -1,6 +1,6 @@
-//SAMPLE
+//SAMPLE HOUSEHOLD
 var houseID = 1;
-//^ REMOVE LATER
+//Big Display Function(Probably need to break it down into seperate functions???)
 function houseDisplay() {
   let queryURL = "/api/household/" + houseID;
   console.log(queryURL);
@@ -21,12 +21,13 @@ function houseDisplay() {
       specialDiv = $("<div>");
       specialDiv.attr("id", "user" + j);
       specialDiv.append("<h3>" + special.name + "</h3>");
+      //Probably not the most efficient way of doing this, but if the person being assigned the chores matches a user, it automatically pushes the chore name into the div that sits underneath the user.
       for (var z = 0; z < rez.Chores.length; z++) {
         var special = rez.Chores[z];
-        console.log(rez.Users[j] + "=" + special.assignedTo);
         if (rez.Users[j].name === special.assignedTo) {
-          specialDiv.append(`<p id='${rez.Users[j].name}
-          ${z}'>${special.name}(${special.frequency})`);
+          var intermediate2 = $(`<p class='chore${z}'>${special.name}(${special.frequency})</p>`);
+          specialDiv.append(intermediate2);
+          console.log(special.isComplete);
         }
       }
       intermediate.append(specialDiv);
@@ -37,6 +38,31 @@ function houseDisplay() {
     householdDiv.append("<h4> Unassigned Chores </h4>");
     householdDiv.append(unAssigned);
     $(".container").append(householdDiv);
+    for (var z = 0; z < rez.Chores.length; z++) {
+      if (rez.Chores[z].isComplete === true) {
+        $(`.chore${z}`).attr("style", "color: green;");
+      }
+    }
+    var hide = false;
+    $("#hideComplete").on("click", function() {
+      if (hide === false) {
+        for (var z = 0; z < rez.Chores.length; z++) {
+          if (rez.Chores[z].isComplete === true) {
+            $(`.chore${z}`).hide();
+          }
+          $("#hideComplete").html("Show Completed Chores");
+        }
+        hide = true;
+      } else {
+        $("#hideComplete").html("Hide Completed Chores");
+        for (var z = 0; z < rez.Chores.length; z++) {
+          if (rez.Chores[z].isComplete === true) {
+            $(`.chore${z}`).show();
+          }
+        }
+        hide = false;
+      }
+    });
   });
 }
 
