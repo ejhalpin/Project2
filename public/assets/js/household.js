6edf6ae20cfe960929cbef755b63c67e041f2819
unknown <flowershirtsman@gmail.com>
2019-08-24 22:17:30 -0400

    chores-now-display-under-users

--- public/assets/js/household.js
+++ public/assets/js/household.js
@@ -10,24 +10,30 @@ function houseDisplay() {
     var householdDiv = $("<div class='households'></div>");
     var rez = response.data;
     householdDiv.append(`<h2>${rez.name}</h2>`);
-    householdDiv.append("<h3>Members</h3>");
+    householdDiv.append("<h2>Members</h2>");
     var houseHolder = $(`<ul id='${rez.name}'> </ul>`);
-
+    var specialDiv;
     var unAssigned = $(`<ul id='unAssigned-${rez.name}'></ul>`);
     for (var j = 0; j < rez.Users.length; j++) {
       var special = rez.Users[j];
-      var intermediate = $(`<li  id ='${special.name}'>${special.name}</li>`);
+      console.log("User name = " + special.name);
+      var intermediate = $("<li>");
+      specialDiv = $("<div>");
+      specialDiv.attr("id", "user" + j);
+      specialDiv.append("<h3>" + special.name + "</h3>");
+      for (var z = 0; z < rez.Chores.length; z++) {
+        var special = rez.Chores[z];
+        console.log(rez.Users[j] + "=" + special.assignedTo);
+        if (rez.Users[j].name === special.assignedTo) {
+          specialDiv.append(`<p id='${rez.Users[j].name}
+          ${z}'>${special.name}(${special.frequency})`);
+        }
+      }
+      intermediate.append(specialDiv);
       houseHolder.append(intermediate);
     }
     houseHolder.append("</ul>");
     householdDiv.append(houseHolder);
-    for (var z = 0; z < rez.Chores.length; z++) {
-      var special = rez.Chores[z];
-      console.log("Special name= " + special.name);
-      console.log("Special frequency=" + special.frequency);
-      var intermediate = $(`<br><p>${special.name}(${special.frequency})</p>`);
-      console.log("#" + special.assignedTo);
-    }
     householdDiv.append("<h4> Unassigned Chores </h4>");
     householdDiv.append(unAssigned);
     $(".container").append(householdDiv);
