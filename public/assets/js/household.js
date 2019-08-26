23cb3c7e756c9c8d691c1c8dfd490d4a1278fdf1
unknown <flowershirtsman@gmail.com>
2019-08-25 23:14:29 -0400

    sorted-chores-by-frequency

--- public/assets/js/household.js
+++ public/assets/js/household.js
@@ -8,51 +8,53 @@ function houseDisplay() {
   $.ajax({ url: queryURL, method: "GET" }).then(function(response) {
     console.log(response.data);
     console.log(response.data.name);
-    var householdDiv = $("<div class='bg-light households'></div>");
     var rez = response.data;
-    householdDiv.append(`<h2>${rez.name}</h2>`);
-    householdDiv.append("<h2>Members</h2>");
-    var houseHolder = $(`<ul id='${rez.name}'> </ul>`);
-    var specialDiv;
-    var unAssigned = $(`<ul id='unAssigned-${rez.name}'></ul>`);
     for (var j = 0; j < rez.Users.length; j++) {
-      var special = rez.Users[j];
-      console.log("User name = " + special.name);
-      var intermediate = $("<li>");
-      specialDiv = $("<div>");
-      specialDiv.attr("id", "user" + j);
-      specialDiv.append("<h3>" + special.name + "</h3>");
-      //Probably not the most efficient way of doing this, but if the person being assigned the chores matches a user, it automatically pushes the chore name into the div that sits underneath the user.
+      var cardDiv = $("<div class='card bg-light mb-3' style='max-width: 20rem;'>");
+      cardDiv.append(`<div class="card-header">${rez.Users[j].name}</div>`);
+      var cardBody = $("<div class='card-body'>");
+      var cardTitle = $("<h5 class='card-title>Chores</h5>");
+      var cardListDaily = $("<ul>");
+      var cardListMonthly = $("<ul>");
+      var cardListWeekly = $("<ul>");
+      var cardListYearly = $("<ul>");
+      cardBody.append(cardTitle);
       for (var z = 0; z < rez.Chores.length; z++) {
         var special = rez.Chores[z];
-        if (rez.Users[j].name === special.assignedTo) {
-          var intermediate2 = $(`<p class='chore${z}'>${special.name}(${special.frequency})</p>`);
-          console.log(special.isComplete);
+        if (rez.Users[j].name === special.assignedTo && special.frequency) {
+          var intermediate2 = $(`<li class='chore${z}'>${special.name}(${special.frequency})</p>`);
           switch (special.frequency) {
             case "dialy":
               intermediate2.addClass("daily");
+              cardListDaily.append(intermediate2);
               break;
             case "weekly":
               intermediate2.addClass("weekly");
+              cardListWeekly.append(intermediate2);
               break;
             case "monthly":
               intermediate2.addClass("monthly");
+              cardListMonthly.append(intermediate2);
               break;
             case "yearly":
               intermediate2.addClass("yearly");
+              cardListYearly.append(intermediate2);
               break;
           }
-          specialDiv.append(intermediate2);
         }
       }
-      intermediate.append(specialDiv);
-      houseHolder.append(intermediate);
+      cardBody.append(cardListDaily);
+      cardBody.append(cardListWeekly);
+      cardBody.append(cardListMonthly);
+      cardBody.append(cardListYearly);
+      cardDiv.append(cardBody);
+      $(".container").append(cardDiv);
     }
-    houseHolder.append("</ul>");
-    householdDiv.append(houseHolder);
-    householdDiv.append("<h4> Unassigned Chores </h4>");
-    householdDiv.append(unAssigned);
-    $(".container").append(householdDiv);
+    // houseHolder.append("</ul>");
+    // householdDiv.append(houseHolder);
+    // householdDiv.append("<h4> Unassigned Chores </h4>");
+    // householdDiv.append(unAssigned);
+    // $(".container").append(householdDiv);
     for (var z = 0; z < rez.Chores.length; z++) {
       if (rez.Chores[z].isComplete === true) {
         $(`.chore${z}`).attr("style", "color: green;");
